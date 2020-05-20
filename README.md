eb7e17a17cfc221a05d3db6004e9eeb021e87ac7
Alex Pinheiro <alexsussa2@gmail.com>
2020-05-20 17:35:29 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # AIDM
 AppImage Desktop Maker
+
+AppImage Desktop Maker (AIDM) é software simples para criação de atalhos desktop para programas .AppImage.
+O mesmo sendo criado com intuito simples e prático.
+
+Modo de uso:
+
+Coloque nome do programa, uma breve descrição, depois selecione o programa com .AppImage e em seguida uma imagem para usar de logo.
+
+Prontinho!
+
+Poderá abrir o menu de programas e buscar pelo nome do programa, abrir e usar tranquilamente.
+
+Claro que como qualquer software em testes e primera versão ainda deve ser implementado.
+
---
+++ aidm.py
@@ -0,0 +1,103 @@
+#!/usr/bin/python3
+# -*- encoding: utf-8 -*-
+
+__author__ = 'Alex Pinheiro'
+__version__ = '0.1'
+__link__ = 'https://github.com/Alexsussa/AIDM'
+
+import gi
+
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk, GObject, Gdk
+import os
+import sys
+
+
+class AppImageDesktopMaker(Gtk.Window):
+    def __init__(self):
+        self.aichooser = builder.get_object('appimagechooser')
+        self.logochooser = builder.get_object('logochooser')
+        self.txtnome = builder.get_object('txtnome')
+        self.txtdescr = builder.get_object('txtdescr')
+        self.txtappimage = builder.get_object('txtappimage')
+        self.txtlogo = builder.get_object('txtlogo')
+        self.loading = builder.get_object('spinner')
+        self.aviso = builder.get_object('aviso')
+        self.lbaviso = builder.get_object('lbaviso')
+        self.sobre = builder.get_object('sobre')
+
+        self.sobre.connect("response", lambda d, r: d.hide())
+
+    def on_btnappimage_clicked(self, button):
+        self.aichooser.show()
+
+    def on_btncancel_clicked(self, button):
+        self.aichooser.hide()
+
+    def on_btnabrir_clicked(self, button):
+        if self.txtappimage.get_text() == '':
+            self.txtappimage.set_text(text=self.aichooser.get_filename())
+            self.aichooser.hide()
+        else:
+            self.txtappimage.set_text(text='')
+            self.txtappimage.set_text(text=self.aichooser.get_filename())
+            self.aichooser.hide()
+
+    def on_btnlogo_clicked(self, button):
+        self.logochooser.show()
+
+    def on_cancelarlogo_clicked(self, button):
+        self.logochooser.hide()
+
+    def on_abrirlogo_clicked(self, button):
+        if self.txtlogo.get_text() == '':
+            self.txtlogo.set_text(text=self.logochooser.get_filename())
+            self.logochooser.hide()
+        else:
+            self.txtlogo.set_text(text='')
+            self.txtlogo.set_text(text=self.logochooser.get_filename())
+            self.logochooser.hide()
+
+    def on_btncriar_clicked(self, button):
+        nome = self.txtnome.get_text()
+        descr = self.txtdescr.get_text()
+        app = self.txtappimage.get_text()
+        img = self.txtlogo.get_text()
+        if nome == '' or descr == '' or app == '' or img == '':
+            self.lbaviso.set_text('Nenhum campo pode estar vázio')
+            self.aviso.show()
+        else:
+            self.loading.start()
+            os.system(f'touch {nome}.desktop')
+            abrir = open(f'{nome}.desktop', mode='w')
+            abrir.write('[Desktop Entry]\n')
+            abrir.write('Version=1.0\n')
+            abrir.write(f'Type=Application\n')
+            abrir.write(f'Terminal=false\n')
+            abrir.write(f'Icon={img}\n')
+            abrir.write(f'Name={nome}\n')
+            abrir.write(f'Exec="{app}"\n')
+            abrir.write(f'Comment={descr}\n')
+            os.system(f'chmod +x {nome}.desktop')
+            os.system(f'mv {nome}.desktop ~/.local/share/applications')
+            self.lbaviso.set_text('Atalho Desktop criado com sucesso.\n'
+                                  'Agora poderá encontrá-lo no menu de aplicações.')
+            self.aviso.show()
+
+    def on_btnok_clicked(self, button):
+        self.aviso.hide()
+        self.loading.stop()
+
+    def on_btnsobre_clicked(self, button):
+        self.sobre.show()
+
+    def on_aidm_destroy(self, window):
+        Gtk.main_quit()
+
+
+builder = Gtk.Builder()
+builder.add_from_file('ui.ui')
+builder.connect_signals(AppImageDesktopMaker())
+janela = builder.get_object('aidm')
+janela.show_all()
+Gtk.main()
